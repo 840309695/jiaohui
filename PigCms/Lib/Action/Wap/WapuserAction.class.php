521739aa08094e8daf969dcac0e6f5e8ecd6cb11
Administrator <Administrator@192.168.191.2>
2015-06-22 19:12:35 +0800

    t

--- PigCms/Lib/Action/Wap/WapuserAction.class.php
+++ PigCms/Lib/Action/Wap/WapuserAction.class.php
@@ -13,8 +13,8 @@ class WapuserAction extends WapAction
    	if($this->isPost()){
    		 if(!empty($_POST)){
    		 	if($userinfo=$this->userdb->where(array("token"=>$this->token,"name"=>$_POST['name'],"password"=>md5($_POST['password'])))->find()){
+   		 		session("wapuid",$userinfo['id']);
    		 	    if(!$userinfo['status']) {$this->error("您的账号还没有通过管理员审核");}	
-   		 	     session("wapuid",$userinfo['id']);
    		 	     $this->success("登录成功",U('Index/index',array("token"=>$this->token,"wecha_id"=>$this->wecha_id)));
    		 	     
    		 	}else{
